53f6e57cfdfa675169836c4d12e525e423ca5d62
RishithaDilshan <87588442+RishithaDilshan@users.noreply.github.com>
2021-07-22 18:06:31 +0530

    Add files via upload

---
+++ arrayfunctionsmap.js
@@ -0,0 +1,15 @@
+
+// code 1
+// const mArr = [1,2,3,4,5];
+
+// let newArr = [] ;
+
+// const doubleValues = (item) => {
+//     return item*2;
+// }
+
+// for(let i=0; i<mArr.length; i++){
+//     newArr.push(doubleValues(mArr[i]))
+// }
+
+// console.log(newArr);
